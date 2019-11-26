2fd4e5399df877eadcc8d3d38bd3913dd486063e
Hiremath <satish.hiremath@capgemini.com>
2019-11-26 13:31:09 +0530

    Springboot Rest H2 Updates

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,2 @@
-create table customer(
-   id long not null auto_increment,
-   name varchar(255) not null,
-   age integer not null,
-   active varchar(255),
-   primary key(id)
-);
-
-insert into customer(id, name, age, active)
-values(1,'Raj', 22, 'false');
-
-insert into customer(id, name, age, active)
-values(2,'Ravi', 23, 'true');
+INSERT INTO EMPLOYEES (first_name, last_name, email) VALUES('Lokesh', 'Gupta', 'will@gmail.com');
+INSERT INTO EMPLOYEES (first_name, last_name, email) VALUES('John', 'Doe', 'xyz@email.com');
